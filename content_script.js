80b7f105906a99b182d2e74c0cd44db2166e5b22
Héctor Palma Téllez <hector@palmatellez.cl>
2016-12-16 18:14:41 -0300

    Initial commit

---
+++ content_script.js
@@ -0,0 +1,36 @@
+(function() {
+  walk(document.body);
+
+  function walk(node) {
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
+
+    var child, next;
+
+    switch ( node.nodeType ) {
+      case 1:  // Element
+      case 9:  // Document
+      case 11: // Document fragment
+
+      child = node.firstChild;
+      while ( child ) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+
+      case 3: // Text node
+      handleText(node);
+      break;
+    }
+  }
+
+  function handleText(textNode) {
+      var v = textNode.nodeValue;
+
+      v = v.replace(/\bfeminazi\b/gi, "UNA FLOR");
+
+      textNode.nodeValue = v;
+  }
+})();
